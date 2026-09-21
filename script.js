08492fc94bb526e0debfe6ff4029d54f8bb59c49
Breno Ponticelli <137233372+brenoiscde@users.noreply.github.com>
2026-09-21 10:56:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+fetch("./mock/consultas.json")
+    .then(response => response.json())
+    .then(dados =>{
+        const consultas = document.querySelector(".lista_consultas");
+        dados.forEach(consulta => {
+            consultas.innerHTML += `
+                <div class="consulta">
+                    <h3>Consulta ${consulta.id}</h3>
+                    <h2>${consulta.Nome}</h2>
+                    <p>${consulta.Descrição}</p>
+                    <p class="abrir_consult">Abrir consulta -></p>
+                </div>
+            `;
+        });
+    })
+    .catch(error => {
+        console.error("Erro:", error);
+    });
+
+fetch("./mock/info.json")
+    .then(response => response.json())
+    .then(dados =>{
+        const informacao = document.querySelector(".informacoes");
+        dados.forEach(info => {
+            informacao.innerHTML += `
+                <div class="info">
+                    <h3>${info.num}</h3>
+                <p>${info.descricao}</p>
+            
+            `
+        })
+    })
+
